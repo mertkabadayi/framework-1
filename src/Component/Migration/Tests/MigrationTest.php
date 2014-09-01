8fc5c4fe5591797a1577e77c7f4b3f8de9979525
Luis Cordova <cordoval@gmail.com>
2014-09-01 18:06:54 -0500

    some more progress

--- src/Component/Migration/Tests/MigrationTest.php
+++ src/Component/Migration/Tests/MigrationTest.php
@@ -52,25 +52,29 @@ class MigrationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(
             [
                 '0000_00_00_000003_test3',
+                '0000_00_00_000002_test2',
+                '0000_00_00_000001_test1',
+                '0000_00_00_000000_init',
             ],
             $migration->get(null, 'down')
         );
-        $this->assertEquals('0000_00_00_000001_test1', $migration->run('0000_00_00_000001_test1'));
+        $this->assertEquals('0000_00_00_000002_test2', $migration->run('0000_00_00_000001_test1'));
 
-        $migration = $this->migrator->create(__DIR__.'/Fixtures', '0000_00_00_000003_test3');
+        $migration = $this->migrator->create(__DIR__.'/Fixtures', '0000_00_00_000004_test4');
         $this->assertEquals(
             [
-                '',
+                '0000_00_00_000004_test4',
+                '0000_00_00_000003_test3',
             ],
-            $migration->get('0000_00_00_000003', 'down')
+            $migration->get('0000_00_00_000002_test2', 'down')
         );
-        $this->assertEquals('0000_00_00_000001_test1', $migration->run('0000_00_00_000001_test1'));
+        $this->assertEquals('0000_00_00_000003_test3', $migration->run('0000_00_00_000002_test2'));
     }
 
     public function testVersion()
     {
         $migration = $this->migrator->create(__DIR__.'/Fixtures');
-        $this->assertEquals(['0000_00_00_000000', '0000_00_00_000001', '0000_00_00_000002'], $migration->run('0000_00_00_000002'));
+        $this->assertEquals('0000_00_00_000000', '0000_00_00_000001', '0000_00_00_000002'], $migration->run('0000_00_00_000002'));
         $this->assertEquals(['0000_00_00_000000'], $migration->run());
 
         $migration = $this->migrator->create(__DIR__.'/Fixtures', '0000_00_00_000002');
