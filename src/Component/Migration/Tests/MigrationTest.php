a8ec5f69e668f928e00542ab7b27f2a05152b681
Luis Cordova <cordoval@gmail.com>
2014-09-01 13:11:43 -0500

    rewrote first testUp to make it consistent with implementation

--- src/Component/Migration/Tests/MigrationTest.php
+++ src/Component/Migration/Tests/MigrationTest.php
@@ -24,15 +24,26 @@ class MigrationTest extends \PHPUnit_Framework_TestCase
     public function testUp()
     {
         $migration = $this->migrator->create(__DIR__.'/Fixtures');
-        $this->assertEquals(['0000_00_00_000000'], $migration->run());
-        $this->assertEquals(['0000_00_00_000000', '0000_00_00_000001', '0000_00_00_000002', '0000_00_00_000003'], $migration->run('0000_00_00_000003'));
+        $this->assertEquals(
+            [
+                '0000_00_00_000000_init',
+                '0000_00_00_000001_test1',
+                '0000_00_00_000002_test2',
+                '0000_00_00_000003_test3',
+            ],
+            $migration->get('0000_00_00_000003_test3', 'up')
+        );
+        $this->assertEquals('0000_00_00_000003_test3', $migration->run('0000_00_00_000003_test3'));
 
-        $migration = $this->migrator->create(__DIR__.'/Fixtures', '0000_00_00_000000');
-        $this->assertEquals(['0000_00_00_000001'], $migration->run());
-        $this->assertEquals(['0000_00_00_000001', '0000_00_00_000002', '0000_00_00_000003'], $migration->run('0000_00_00_000003'));
-
-        $migration = $this->migrator->create(__DIR__.'/Fixtures', '0000_00_00_000003');
-        $this->assertCount(0, $migration->run('0000_00_00_000003'));
+        $migration = $this->migrator->create(__DIR__.'/Fixtures', '0000_00_00_000003_test3');
+        $this->assertEquals(
+            [
+                '0000_00_00_000004_test4',
+                '0000_00_00_000005_test5',
+            ],
+            $migration->get('0000_00_00_000005_test5', 'up')
+        );
+        $this->assertEquals('0000_00_00_000005_test5', $migration->run('0000_00_00_000005_test5'));
     }
 
     public function testDown()
