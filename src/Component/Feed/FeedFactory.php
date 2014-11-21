3c21c4ef47b98599bad3b606bfeff68e9d6c6256
janschoenherr <jan@yootheme.com>
2014-11-21 17:15:48 +0100

    add custom feed types

--- src/Component/Feed/FeedFactory.php
+++ src/Component/Feed/FeedFactory.php
@@ -2,27 +2,41 @@
 
 namespace Pagekit\Component\Feed;
 
-use Pagekit\Component\Feed\Feed\Atom;
-use Pagekit\Component\Feed\Feed\RSS1;
-use Pagekit\Component\Feed\Feed\RSS2;
-
 class FeedFactory
 {
     /**
+     * @var string[]
+     */
+    protected $feeds = [
+        Feed::ATOM => 'Pagekit\Component\Feed\Feed\Atom',
+        Feed::RSS1 => 'Pagekit\Component\Feed\Feed\RSS1',
+        Feed::RSS2 => 'Pagekit\Component\Feed\Feed\RSS2'
+    ];
+
+    /**
      * Creates a feed.
      *
-     * @param string $type
+     * @param  string $type
      * @return FeedInterface
      */
     public function create($type = null)
     {
-        switch ($type) {
-            case Feed::ATOM:
-                return new Atom;
-            case Feed::RSS1:
-                return new RSS1;
-            default:
-                return new RSS2;
+        $class = isset($this->feeds[$type]) ? $this->feeds[$type] : $this->feeds[Feed::RSS2];
+        return new $class;
+    }
+
+    /**
+     * Registers a new feed type.
+     *
+     * @param string $type
+     * @param string $class
+     */
+    public function register($type, $class)
+    {
+        if (!is_string($class) || !is_subclass_of($class, 'Pagekit\Component\Feed\FeedInterface')) {
+            throw new \InvalidArgumentException(sprintf('Given type class "%s" is not of type Pagekit\Component\Feed\FeedInterface', (string) $class));
         }
+
+        $this->feeds[$type] = $class;
     }
 }
